8dd75a2614e6eb26aad92a9fb7660e26e776d2b2
Patrick B <zugamifk@gmail.com>
2015-11-15 19:54:29 -0400

    loading bar art and gui addedd

---
+++ Assets/Albedo/Scripts/Editor/TextureImportSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using Albedo;
+
+namespace Albedo.Editor {
+	public class TextureImportSettings : AssetPostprocessor
+	{
+		void OnPostprocessTexture(Texture2D texture)
+		{
+			TextureImporter importer = assetImporter as TextureImporter;
+			importer.anisoLevel = 0;
+			importer.filterMode = FilterMode.Point;
+            importer.mipmapEnabled = false;
+			importer.spritePixelsPerUnit = Constants.SpritePPU;
+            importer.textureFormat = TextureImporterFormat.RGBA32;
+
+        }
+	}
+}
---
+++ Assets/Albedo/Scripts/Graphics/LoadingBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace Albedo.Graphics {
+	public class LoadingBar : MonoBehaviour {
+
+        [SerializeField]
+        Text m_statusText;
+        [SerializeField]
+        Image m_loadingBar;
+
+        List<LoadJob> jobs;
+        int totalSize;
+
+		private struct LoadJob {
+            public int weight;
+            public System.Func<float> statusCB;
+        }
+
+        public void AddJob(int weight, System.Func<float> statusCB) {
+            jobs.Add(new LoadJob{weight = weight, statusCB = statusCB});
+			totalSize += weight;
+        }
+
+        void Awake() {
+            m_statusText.text = "Loading";
+            m_loadingBar.fillAmount = 0;
+        }
+	}
+}
